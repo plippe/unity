55768fc24a0a7891924aa20d1a61fd2f8ed09db3
Philippe Vinchon <p.vinchon@gmail.com>
2014-11-16 21:34:51 +0000

    added easy exit to leave the game

---
+++ sebastianlague/first-person-controller-spherical-worlds/Assets/Scripts/Game.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape)) {
+			print ("Should exit");
+			Application.Quit();
+		}
+	}
+}
