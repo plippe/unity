e472e8c9ab29c9ba1d2e55bfceb3a097757cf294
Philippe Vinchon <p.vinchon@gmail.com>
2015-10-29 17:37:41 +0000

    Implement state behaviour

--- official/state-machine/Assets/_CompletedAssets/Scripts/StateMachines/ChaseState.cs
+++ official/state-machine/Assets/_CompletedAssets/Scripts/StateMachines/ChaseState.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class ChaseState : IEnemyState
 {
+    private readonly StatePatternEnemy enemy;
+
     public ChaseState(StatePatternEnemy parent)
     {
-
+        enemy = parent;
     }
 
     public void Update()
     {
-
+        Look();
+        Chase();
     }
 
     public void OnTriggerEnter(Collider other)
@@ -20,16 +23,39 @@ public class ChaseState : IEnemyState
 
     public void ToPatrolState()
     {
-
+        enemy.currentState = enemy.patrolState;
     }
 
     public void ToAlertState()
     {
-
+        enemy.currentState = enemy.alertState;
     }
 
     public void ToChaseState()
     {
+        enemy.currentState = enemy.chaseState;
+    }
 
+    private void Look()
+    {
+        RaycastHit hit;
+        Vector3 enemyToTarget = (enemy.chaseTarget.position + enemy.offset) - enemy.eyes.transform.position;
+        if (Physics.Raycast(enemy.eyes.transform.position, enemyToTarget, out hit, enemy.sightRange)
+            && hit.collider.CompareTag("Player"))
+        {
+            enemy.chaseTarget = hit.transform;
+        }
+        else
+        {
+            ToAlertState();
+        }
+    }
+
+    private void Chase()
+    {
+        enemy.meshRenderedFlag.material.color = Color.red;
+
+        enemy.navMeshAgent.destination = enemy.chaseTarget.position;
+        enemy.navMeshAgent.Resume();
     }
 }
--- official/state-machine/Assets/_CompletedAssets/Scripts/StateMachines/PatrolState.cs
+++ official/state-machine/Assets/_CompletedAssets/Scripts/StateMachines/PatrolState.cs
@@ -3,33 +3,64 @@ using System.Collections;
 
 public class PatrolState : IEnemyState
 {
+    private readonly StatePatternEnemy enemy;
+    private int nextWaypoint;
+
     public PatrolState(StatePatternEnemy parent)
     {
-
+        enemy = parent;
     }
 
     public void Update()
     {
-
+        Look();
+        Patrol();
     }
 
     public void OnTriggerEnter(Collider other)
     {
-
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ToAlertState();
+        }
     }
 
     public void ToPatrolState()
     {
-
+        enemy.currentState = enemy.patrolState;
     }
 
     public void ToAlertState()
     {
-
+        enemy.currentState = enemy.alertState;
     }
 
     public void ToChaseState()
     {
+        enemy.currentState = enemy.chaseState;
+    }
 
+    private void Look()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(enemy.eyes.transform.position, enemy.eyes.transform.forward, out hit, enemy.sightRange)
+            && hit.collider.CompareTag("Player"))
+        {
+            enemy.chaseTarget = hit.transform;
+            ToChaseState();
+        }
+    }
+
+    private void Patrol()
+    {
+        enemy.meshRenderedFlag.material.color = Color.green;
+
+        enemy.navMeshAgent.destination = enemy.waypoints[nextWaypoint].position;
+        enemy.navMeshAgent.Resume();
+
+        if (enemy.navMeshAgent.remainingDistance <= enemy.navMeshAgent.stoppingDistance)
+        {
+            nextWaypoint = (nextWaypoint + 1) % enemy.waypoints.Length;
+        }
     }
 }
