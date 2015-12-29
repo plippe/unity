735bb3e57c1b2750b8527eaf69d398070e9295c1
Philippe Vinchon <philippe@intercom.io>
2015-12-29 11:38:19 +0000

    Event manager test

---
+++ official/messaging-system/Assets/Scripts/Test1Trigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test1Trigger : MonoBehaviour {
+	void Update () {
+		if (Input.GetKeyDown ("q")) {
+			EventManager.TriggerEvent("test");
+		}
+	}
+}
