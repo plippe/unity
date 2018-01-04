02b4c45fbef557713273b7cd9799626c1e3bc4e2
Philippe Vinchon <p.vinchon@gmail.com>
2018-01-04 13:44:20 +0000

    Number wizard ui from udemy

---
+++ learn-to-code-by-making-games/number-wizard-ui/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SceneManager))]
+public class NumberWizard : MonoBehaviour
+{
+    public Text question;
+    public int max = 1000;
+    public int min = 1;
+
+    int guess;
+    bool isFinalGuess = false;
+
+    SceneManager sceneManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        sceneManager = GetComponent<SceneManager>();
+        Guess(min, max);
+    }
+
+    int Mid(int min, int max)
+    {
+        return (max + min) / 2;
+    }
+
+    int GuessNumber(int min, int max)
+    {
+        int mid = Mid(min, max);
+        return Random.Range(
+            Mathf.Max(min, mid - 10),
+            Mathf.Min(max, mid + 10)
+        );
+    }
+
+    void Guess(int min, int max)
+    {
+        if (min == max)
+        {
+            isFinalGuess = true;
+            question.text = "Is your number " + min + "?";
+        }
+        else
+        {
+            guess = GuessNumber(min, max);
+            question.text = "Is your number higher to " + guess + "?";
+        }
+    }
+
+    public void Yes() { Answer(true); }
+    public void No() { Answer(false); }
+    public void Answer(bool yes)
+    {
+        if (isFinalGuess)
+        {
+            string endScene = yes ? "Win" : "Lose";
+            sceneManager.LoadScene(endScene);
+        }
+        else
+        {
+            if (yes)
+            {
+                min = guess + 1;
+            }
+            else
+            {
+                max = guess;
+            }
+
+            Guess(min, max);
+        }
+    }
+
+}
---
+++ learn-to-code-by-making-games/number-wizard-ui/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public void LoadScene(string sceneName)
+    {
+        UnitySceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
