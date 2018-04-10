9b89154ddc1981442bcddbec98bba353052ad819
Philippe Vinchon <p.vinchon@gmail.com>
2018-04-10 22:08:57 +0100

    WIP

---
+++ learn-to-code-by-making-games/block-breaker/Assets/BallOutOfBounds/BallOutOfBounds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallOutOfBounds : MonoBehaviour {
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("MainMenu/MainMenu");
+
+    }
+
+}
---
+++ learn-to-code-by-making-games/block-breaker/Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstSceneName;
+
+    public void OnStartClick()
+    {
+        Debug.Log("Starting Game");
+        SceneManager.LoadScene(firstSceneName);
+    }
+
+    public void OnQuitClick()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+
+}
